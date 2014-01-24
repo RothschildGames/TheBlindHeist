4c3823a8227a3264144cd234fbd7371138fb01d9
yon <Lighthawky@gmail.com>
2014-01-24 16:52:32 +0200

    removed SHIT!

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -6,24 +6,26 @@ public class CameraManager : MonoBehaviour {
 
 	public Camera playerCamera;
 	public List<Camera> cameras;
-//	private KeyCode[] keyCodes = {KeyCode.Alpha0, KeyCode.Alpha1, };
+
+	private Camera current;
 
 	// Use this for initialization
 	void Start () {
-	
+		current = playerCamera;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
 		if (Input.GetKey (KeyCode.Alpha0)) {
-			Camera.current.enabled = false;
+			current.enabled = false;
 			playerCamera.enabled = true;
+			current = playerCamera;
 		} else {
 			for (int i=0; i < cameras.Count; i++) {
 				if (Input.GetKey (KeyCode.Alpha1 + i)){
-					Camera.current.enabled = false;
+					current.enabled = false;
 					cameras[i].enabled = true;
+					current = cameras[i];
 				}
 			}
 		}
