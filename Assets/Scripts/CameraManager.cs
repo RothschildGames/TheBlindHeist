6df3dc9bb400331d16d3770f831422e07c7e6d6f
yon <Lighthawky@gmail.com>
2014-01-24 15:42:12 +0200

    added camera manager

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class CameraManager : MonoBehaviour {
+
+
+	public Camera playerCamera;
+	public List<Camera> cameras;
+//	private KeyCode[] keyCodes = {KeyCode.Alpha0, KeyCode.Alpha1, };
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey (KeyCode.Alpha0)) {
+			Camera.current.enabled = false;
+			playerCamera.enabled = true;
+		} else {
+			for (int i=0; i < cameras.Count; i++) {
+				if (Input.GetKey (KeyCode.Alpha1 + i)){
+					Camera.current.enabled = false;
+					cameras[i].enabled = true;
+				}
+			}
+		}
+	}
+}
