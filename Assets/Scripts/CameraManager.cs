e77d273d3e933217416c9fb47d304fb24d74e7c6
misha <mishas@mishas-macbookpro.roam.corp.google.com>
2014-01-24 21:14:09 +0200

    Added empty cam

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -6,18 +6,16 @@ public class CameraManager : MonoBehaviour {
 
 	public Camera playerCamera;
 	public List<SecurityCamera> cameras;
+	public SecurityCamera noiseCamera;
+
+	private List<SecurityCamera> noiseCameras= new List<SecurityCamera>();
 
 	private Rect[] rectPositions = {
 		new Rect (0, Screen.height / 2, Screen.width / 2, Screen.height / 2),
 		new Rect (Screen.width / 2, Screen.height / 2, Screen.width / 2, Screen.height / 2),
 		new Rect (Screen.width / 2, 0, Screen.width / 2, Screen.height / 2)
 	};
-
-	private Rect[] guiPositions = {
-		new Rect (0, 0, Screen.width / 2, Screen.height / 2),
-		new Rect (Screen.width / 2, 0, Screen.width / 2, Screen.height / 2),
-		new Rect (Screen.width / 2, Screen.height / 2, Screen.width / 2, Screen.height / 2)
-	};
+	
 
 	private SecurityCamera[] currentCameras;
 	private int lastCamera;
@@ -28,27 +26,31 @@ public class CameraManager : MonoBehaviour {
 		currentCameras = new SecurityCamera[rectPositions.Length];
 		lastCamera = currentCameras.Length - 1;
 		for (int i = 0; i < currentCameras.Length; i++) {
+			SecurityCamera c = (SecurityCamera) SecurityCamera.Instantiate (noiseCamera);
+			c.camera.pixelRect = rectPositions[i];
+			noiseCameras.Add (c);
 			setCamera (cameras [i]);
 		}
-
 	}
-
 	
 	// Update is called once per frame
 	void Update () {
 		for (int i=0; i < cameras.Count; i++) {
-			if (Input.GetKeyUp (KeyCode.Alpha1 + i) && !isCurrent(cameras[i])){
+			if (Input.GetKeyUp (KeyCode.Alpha1 + i)) {
+				int currCamLocation = findCurrCamera(cameras[i]);
+				if (currCamLocation != -1) {
+					setNoiseCamera(currCamLocation);
+				}
 				setCamera(cameras[i]);
 			}
 		}
 	}
 
-	void OnGUI()
-	{
-		for (int i = 0; i < currentCameras.Length; i++) {
-			int index = cameras.IndexOf(currentCameras[i]) + 1;
-			GUI.Label (guiPositions[i], index.ToString());
-		}
+	void setNoiseCamera(int currCamLocation) {
+		currentCameras[currCamLocation].controlable = false;
+		currentCameras[currCamLocation].camera.enabled = false;
+		currentCameras[currCamLocation] = noiseCameras[currCamLocation];
+		noiseCameras[currCamLocation].camera.enabled = true;
 	}
 
 	void setCamera (SecurityCamera c) {
@@ -62,10 +64,10 @@ public class CameraManager : MonoBehaviour {
 		lastCamera = nextCamera;
 	}
 
-	bool isCurrent (SecurityCamera c){
+	int findCurrCamera (SecurityCamera c) {
 		for (int i =0 ; i < currentCameras.Length ; i++){
-			if (currentCameras[i] == c) return true;
+			if (currentCameras[i] == c) return i;
 		}
-		return false;
+		return -1;
 	}
 }
