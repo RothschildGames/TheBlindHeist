cc69f263b8c274bb119b797177489209089551e4
yon <Lighthawky@gmail.com>
2014-01-24 22:20:44 +0200

    added hacking movie

---
+++ Assets/Scripts/PlayVideo.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayVideo : MonoBehaviour {
+
+	public MovieTexture movieTx;
+	public AudioClip audioSrc;
+
+	// Use this for initialization
+	void Start () {
+		movieTx = guiTexture.texture as MovieTexture;
+		audioSrc = movieTx.audioClip;
+
+//		movieTx.audioClip
+
+//		guiTexture.audio.pitch = 2.0f;
+
+//		audioSrc.g
+//		audioSrc.pitch = 2.0f;
+		movieTx.loop = true;
+
+		movieTx.Play ();
+//		audioSrc.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
