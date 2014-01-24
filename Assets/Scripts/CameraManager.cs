17d8ee10c3c1f8a44f15e72fa3af10a79a170ce3
yon <Lighthawky@gmail.com>
2014-01-24 20:30:04 +0200

    cameras are changable

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -7,58 +7,51 @@ public class CameraManager : MonoBehaviour {
 	public Camera playerCamera;
 	public List<SecurityCamera> cameras;
 
+	private Rect[] rectPositions = {
+		new Rect (0, Screen.height / 2, Screen.width / 2, Screen.height / 2),
+		new Rect (Screen.width / 2, Screen.height / 2, Screen.width / 2, Screen.height / 2),
+		new Rect (Screen.width / 2, 0, Screen.width / 2, Screen.height / 2)
+	};
+	
+
+	private SecurityCamera[] currentCameras;
+	private int lastCamera;
+
 	// Use this for initialization
 	void Start () {
+		currentCameras = new SecurityCamera[rectPositions.Length];
+		lastCamera = currentCameras.Length - 1;
+		for (int i = 0; i < currentCameras.Length; i++) {
+			setCamera (cameras [i]);
+		}
+
 	}
+
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyUp (KeyCode.Alpha0)) {
-			bringToFront(playerCamera);
-		} else {
-			for (int i=0; i < cameras.Count; i++) {
-				if (Input.GetKeyUp (KeyCode.Alpha1 + i)){
-					bringToFront(cameras[i].camera);
-				}
-			}
-			if (Input.GetKeyUp (KeyCode.Equals)){
-				playerCamera.enabled = true;
-				playerCamera.camera.enabled = false;
-				playerCamera.pixelRect = new Rect(0,0,Screen.width/2, Screen.height/2);
-
-				cameras[0].camera.enabled = true;
-				cameras[0].controlable = false;
-				cameras[0].camera.pixelRect = new Rect(0,Screen.height/2,Screen.width/2, Screen.height/2);
-
-				cameras[1].camera.enabled = true;
-				cameras[1].controlable = false;
-				cameras[1].camera.pixelRect = new Rect(Screen.width/2,Screen.height/2,Screen.width/2, Screen.height/2);
-
-
-				cameras[2].camera.enabled = true;
-				cameras[2].controlable = false;
-				cameras[2].camera.pixelRect = new Rect(Screen.width/2,0,Screen.width/2, Screen.height/2);
-			}
-		}
-	}
-
-	void bringToFront (Camera c){
-		disableAll ();
-//		c.enabled = true;
-		c.camera.enabled = true;
-		if (c.GetComponent<SecurityCamera> ()) {
-			c.GetComponent<SecurityCamera> ().controlable = true;
-		}
-	}
-
-	void disableAll(){
-		playerCamera.pixelRect = new Rect(0,0,Screen.width, Screen.height);
-		playerCamera.camera.enabled = false;
 		for (int i=0; i < cameras.Count; i++) {
-			cameras[i].camera.enabled = false;
-			cameras[i].controlable = false;;
-			cameras[i].camera.pixelRect = new Rect(0,0,Screen.width, Screen.height);
+			if (Input.GetKeyUp (KeyCode.Alpha1 + i) && !isCurrent(cameras[i])){
+				setCamera(cameras[i]);
+			}
 		}
+	}
 
+	void setCamera (SecurityCamera c) {
+		int nextCamera = (lastCamera + 1) % currentCameras.Length;
+		if (currentCameras [nextCamera]) currentCameras [nextCamera].camera.enabled = false;
+		if (currentCameras [lastCamera]) currentCameras [lastCamera].controlable = false;
+		c.camera.pixelRect = rectPositions [nextCamera];
+		c.camera.enabled = true;
+		c.controlable = true;
+		currentCameras [nextCamera] = c;
+		lastCamera = nextCamera;
+	}
+
+	bool isCurrent (SecurityCamera c){
+		for (int i =0 ; i < currentCameras.Length ; i++){
+			if (currentCameras[i] == c) return true;
+		}
+		return false;
 	}
 }
