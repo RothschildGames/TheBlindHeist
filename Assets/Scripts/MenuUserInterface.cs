5ccb035201524b3e9de1db1ce40be706f997b1ae
Shay Davidson <shay.h.davidson@gmail.com>
2014-01-24 18:54:36 +0200

    Added interface

--- Assets/Scripts/MenuUserInterface.cs
+++ Assets/Scripts/MenuUserInterface.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class MenuUserInterface : MonoBehaviour {
-
+	
 	// Use this for initialization
 	void Start () {
 	
@@ -11,5 +11,21 @@ public class MenuUserInterface : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void OnGUI () {
+
+
+		if(GUI.Button(new Rect(325,200,300,60), "Start Local Game")) {
+			Application.LoadLevel(1);
+		}
+	
+		if(GUI.Button(new Rect(325,300,300,60), "Host Server")) {
+			Application.LoadLevel(1);
+		}
+
+		if(GUI.Button(new Rect(325,400,300,60), "Join Existing Gamer")) {
+			Application.LoadLevel(2);
+		}
 	}
 }
