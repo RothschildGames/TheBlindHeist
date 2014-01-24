1b9245e692999579a03f879a2c8865c55ef67d05
Noam Gat <noamgat@gmail.com>
2014-01-24 16:10:36 +0200

    Added a basic win condition + game logic / interface + main menu

---
+++ Assets/Scripts/GameUserInterface.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameUserInterface : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GameLogicManager logicManager = GameLogicManager.singletonInstance;
+        if (logicManager.wonGame || logicManager.lostGame)
+        {
+            guiText.enabled = true;
+            if (logicManager.wonGame)
+            {
+                guiText.text = "You win!";
+            }
+            else
+            {
+                guiText.text = @"You lose!";
+            }
+            StartCoroutine(ReturnToMainMenuAfterDelay());
+        }
+	}
+
+    IEnumerator ReturnToMainMenuAfterDelay()
+    {
+        yield return new WaitForSeconds(3);
+        Application.LoadLevel("MainMenuScene");
+    }
+}
