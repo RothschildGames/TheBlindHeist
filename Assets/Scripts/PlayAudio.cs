db837204ee8ef55f550c77fbadc80c61ff27fb6a
misha <mishas@mishas-macbookpro.roam.corp.google.com>
2014-01-25 13:53:34 +0200

    Added music

---
+++ Assets/Scripts/PlayAudio.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayAudio : MonoBehaviour {
+
+	public AudioSource intro;
+	public AudioSource loop;
+	public AudioSource win;
+	public AudioSource lose;
+
+	private bool gameStarted = false;
+	private bool endOfGame = false;
+	private bool isVictory = false;
+	private bool finalSequenceDone = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (gameStarted && !endOfGame && !intro.isPlaying && !loop.isPlaying) {
+			loop.Play();
+		}
+		if (endOfGame && !loop.isPlaying && !finalSequenceDone) {
+			if (isVictory) {
+				win.Play ();
+			} else {
+				lose.Play ();
+			}
+			finalSequenceDone = true;
+		}
+	}
+
+	public void StartGame() {
+		intro.Play();
+		gameStarted = true;
+	}
+
+	public void lost() {
+		if (!gameStarted) return;
+		endOfGame = true;
+		loop.loop = false;
+		isVictory = false;
+	}
+
+	public void won() {
+		if (!gameStarted) return;
+		endOfGame = true;
+		loop.loop = false;
+		isVictory = true;
+	}
+}
