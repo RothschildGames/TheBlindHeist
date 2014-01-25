3ea52450364e34bc162f5c977e5431b8387152e1
sevyatar <sevyatar@gmail.com>
2014-01-25 15:52:20 +0200

    When opening a new server, the number of currently connected users is now shown

--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -42,6 +42,10 @@ public class NetworkManager : MonoBehaviour {
 			Debug.Log ("server registered");
 	}
 
+	public int CountConnectedUsers()
+	{
+		return Network.connections.Length;
+	}
 
 
 	//******** client code
