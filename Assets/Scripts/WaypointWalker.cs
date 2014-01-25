2318f7423c3efd29e85dc117b00bd5f0f2649bd4
Noam Gat <noamgat@gmail.com>
2014-01-25 12:28:19 +0200

    Guards now take time to turn

--- Assets/Scripts/WaypointWalker.cs
+++ Assets/Scripts/WaypointWalker.cs
@@ -6,6 +6,7 @@ public class WaypointWalker : MonoBehaviour {
     public Transform[] waypoints;
     public bool cyclic;
     public float movementSpeed = 1;
+    public float rotationSpeed = 90;
 
     private int lastWaypointHit = 0;
     private int waypointDirection = 1;
@@ -51,17 +52,29 @@ public class WaypointWalker : MonoBehaviour {
             waypointDirection = -1;
         }
         transform.position = CurrentWaypoint.position;
-        transform.LookAt(TargetWaypoint);
+        //transform.LookAt(TargetWaypoint);
     }
 	
 	// Update is called once per frame
 	void Update () {
-        transform.position = Vector3.MoveTowards(transform.position, 
-            TargetWaypoint.position, Time.deltaTime * movementSpeed);
-        //Debug.Log("Distance to target : " + (TargetWaypoint.position - transform.position).magnitude.ToString());
-        if ((TargetWaypoint.position - transform.position).magnitude < 0.01f)
+        float angleFromTarget = Vector3.Angle(transform.forward, (TargetWaypoint.position - transform.position));
+        if (angleFromTarget > 0.1f)
         {
-            HitWaypoint();
+            //Debug.Log("Angle is " + angleFromTarget.ToString() + ", rotating");
+            Quaternion curOrientation = transform.rotation;
+            transform.LookAt(TargetWaypoint.position);
+            Quaternion targetOrientation = transform.rotation;
+            transform.rotation = Quaternion.RotateTowards(curOrientation, targetOrientation, rotationSpeed * Time.deltaTime);
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position,
+            TargetWaypoint.position, Time.deltaTime * movementSpeed);
+            //Debug.Log("Distance to target : " + (TargetWaypoint.position - transform.position).magnitude.ToString());
+            if ((TargetWaypoint.position - transform.position).magnitude < 0.01f)
+            {
+                HitWaypoint();
+            }
         }
 	}
 }
