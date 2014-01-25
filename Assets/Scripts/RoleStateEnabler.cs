d8f806c83848cdc90f016226a5beb0ffae870d91
Noam Gat <noamgat@gmail.com>
2014-01-25 10:57:06 +0200

    Adding role management classes to help disable / enable classes

---
+++ Assets/Scripts/RoleStateEnabler.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoleStateEnabler : MonoBehaviour {
+
+    public bool allowRunner;
+    public bool allowHacker;
+    
+    public GameObject[] gameObjects;
+    public Behaviour[] components;
+
+    private bool IsAllowed
+    {
+        get
+        {
+            if (GameRole.singletonInstance.IsRunner)
+            {
+                return allowRunner;
+            }
+            else
+            {
+                return allowHacker;
+            }
+        }
+    }
+
+    void Awake()
+    {
+        foreach (GameObject go in gameObjects)
+        {
+            go.SetActive(IsAllowed);
+        }
+        foreach (Behaviour c in components)
+        {
+            c.enabled = IsAllowed;
+        }
+    }
+}
